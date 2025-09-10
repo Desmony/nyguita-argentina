cf0510417a5034cff922fd174f542c2bda2dfc31
Desmony <33091422+Desmony@users.noreply.github.com>
2025-09-10 14:20:57 +0200

    Civic modifiers decriptions

--- progression-trees/nyguita-argentina/localizations.sql
+++ progression-trees/nyguita-argentina/localizations.sql
@@ -10,3 +10,37 @@ VALUES ('en_US',  'LOC_TREE_NYGUITA_ARGENTINA_NAME', 'Angentinian Civics Tree'),
        ('en_US',  'LOC_CIVIC_NYGUITA_CIVILIZATION_AND_BARBARISM_AUTHOR', 'Domingo Faustino Sarmiento'),
        ('en_US',  'LOC_CIVIC_NYGUITA_TANGO_AND_POETRY_QUOTE', 'A writer — and, I believe, generally all persons — must think that whatever happens to him or her is a resource. All things have been given to us for a purpose, and an artist must feel this more intensely.'),
        ('en_US',  'LOC_CIVIC_NYGUITA_TANGO_AND_POETRY_AUTHOR', 'Jorge Luis Borges');
+
+INSERT INTO
+  LocalizedText (Tag, Language, Text)
+VALUES
+       (
+    'LOC_MOD_NYGUITA_GREAT_EUROPEAN_MIGRATION_WAVE_DESCRIPTION',
+    'en_US',
+    '+15% Growth in all settlements.'
+  ),
+  (
+    'LOC_MOD_NYGUITA_VASTNESS_OF_THE_PAMPAS_DESCRIPTION',
+    'en_US',
+    '+1 [icon:SETTLEMENT_LIMIT] Settlement limit.'
+  ),
+  (
+    'LOC_MOD_NYGUITA_LIGA_DE_GOBERNADORES_DESCRIPTION',
+    'en_US',
+    '+5 [icon:YIELD_GOLD] Gold per Tradition slotted.'
+  ),
+  (
+    'LOC_MOD_NYGUITA_PATRIA_GRANDE_DESCRIPTION',
+    'en_US',
+    '+1 [icon:SETTLEMENT_LIMIT] Settlement limit.'
+  ),
+  (
+    'LOC_MOD_NYGUITA_FLORIDA_AND_BOEDO_DESCRIPTION',
+    'en_US',
+    '+5 [icon:YIELD_CULTURE] Culture per non-Tradition slotted.'
+  ),
+  (
+    'LOC_MOD_NYGUITA_GIANTS_OF_PATAGONIA_DESCRIPTION',
+    'en_US',
+    '+30% [icon:YIELD_PRODUCTION] Production towards Archeologists. +3 [icon:YIELD_SCIENCE] Science on [icon:GREATWORK] Artifacts.'
+  );
--- progression-trees/nyguita-argentina/modern.sql
+++ progression-trees/nyguita-argentina/modern.sql
@@ -42,6 +42,10 @@ INSERT INTO ModifierArguments
        (ModifierId,                                  Name,      Value) 
 VALUES ('MOD_NYGUITA_GREAT_EUROPEAN_MIGRATION_WAVE', 'Percent', '15');
 
+INSERT INTO ModifierStrings 
+       (ModifierId,              Context,       Text) 
+VALUES ('MOD_NYGUITA_GREAT_EUROPEAN_MIGRATION_WAVE', 'Description', 'LOC_MOD_NYGUITA_GREAT_EUROPEAN_MIGRATION_WAVE_DESCRIPTION');
+
 
 -- MOD_NYGUITA_VASTNESS_OF_THE_PAMPAS
 INSERT INTO Types 
@@ -60,6 +64,10 @@ INSERT INTO ModifierArguments
        (ModifierId,                           Name,     Value) 
 VALUES ('MOD_NYGUITA_VASTNESS_OF_THE_PAMPAS', 'Amount', '1');
 
+INSERT INTO ModifierStrings 
+       (ModifierId,              Context,       Text) 
+VALUES ('MOD_NYGUITA_VASTNESS_OF_THE_PAMPAS', 'Description', 'LOC_MOD_NYGUITA_VASTNESS_OF_THE_PAMPAS_DESCRIPTION');
+
 
 -- CIVIC_NYGUITA_CIVILIZATION_AND_BARBARISM
 INSERT INTO Types 
@@ -72,15 +80,9 @@ VALUES ('CIVIC_NYGUITA_CIVILIZATION_AND_BARBARISM', 'TREE_NYGUITA_ARGENTINA', 30
 
 INSERT INTO ProgressionTreeNodeUnlocks 
        (ProgressionTreeNodeType,                    TargetType,                                 Hidden, TargetKind,       UnlockDepth) 
-VALUES ('CIVIC_NYGUITA_CIVILIZATION_AND_BARBARISM', 'TRADITION_NYGUITA_CONQUEST_OF_THE_DESERT', 0,      'KIND_TRADITION', 1);
-
-INSERT INTO ProgressionTreeNodeUnlocks 
-       (ProgressionTreeNodeType,                    TargetType,                      Hidden, TargetKind,           UnlockDepth) 
-VALUES ('CIVIC_NYGUITA_CIVILIZATION_AND_BARBARISM', 'BUILDING_NYGUITA_BANCO_NACION', 0,      'KIND_CONSTRUCTIBLE', 1);
-
-INSERT INTO ProgressionTreeNodeUnlocks 
-       (ProgressionTreeNodeType,                    TargetType,                  Hidden, TargetKind,      UnlockDepth) 
-VALUES ('CIVIC_NYGUITA_CIVILIZATION_AND_BARBARISM', 'MOD_NYGUITA_PATRIA_GRANDE', 0,      'KIND_MODIFIER', 2);
+VALUES ('CIVIC_NYGUITA_CIVILIZATION_AND_BARBARISM', 'TRADITION_NYGUITA_CONQUEST_OF_THE_DESERT', 0,      'KIND_TRADITION', 1),
+       ('CIVIC_NYGUITA_CIVILIZATION_AND_BARBARISM', 'BUILDING_NYGUITA_BANCO_NACION', 0,      'KIND_CONSTRUCTIBLE', 1),
+       ('CIVIC_NYGUITA_CIVILIZATION_AND_BARBARISM', 'MOD_NYGUITA_PATRIA_GRANDE', 0,      'KIND_MODIFIER', 2);
 
 
 -- MOD_NYGUITA_PATRIA_GRANDE
@@ -99,6 +101,10 @@ VALUES ('MOD_NYGUITA_PATRIA_GRANDE', 'MOD_NYGUITA_PATRIA_GRANDE_TYPE');
 INSERT INTO ModifierArguments 
        (ModifierId,                  Name,     Value) 
 VALUES ('MOD_NYGUITA_PATRIA_GRANDE', 'Amount', '1');
+
+INSERT INTO ModifierStrings 
+       (ModifierId,              Context,       Text) 
+VALUES ('MOD_NYGUITA_PATRIA_GRANDE', 'Description', 'LOC_MOD_NYGUITA_PATRIA_GRANDE_DESCRIPTION');
 
 INSERT INTO ProgressionTreeNodeUnlocks 
        (ProgressionTreeNodeType,                    TargetType,                         Hidden, TargetKind,      UnlockDepth) 
@@ -120,15 +126,13 @@ VALUES ('MOD_NYGUITA_LIGA_DE_GOBERNADORES', 'MOD_NYGUITA_LIGA_DE_GOBERNADORES_TY
 
 INSERT INTO ModifierArguments 
        (ModifierId,                         Name,        Value) 
-VALUES ('MOD_NYGUITA_LIGA_DE_GOBERNADORES', 'YieldType', 'YIELD_GOLD');
+VALUES ('MOD_NYGUITA_LIGA_DE_GOBERNADORES', 'YieldType', 'YIELD_GOLD'),
+       ('MOD_NYGUITA_LIGA_DE_GOBERNADORES', 'Amount', '5'),
+       ('MOD_NYGUITA_LIGA_DE_GOBERNADORES', 'CivUnique', 'True');
 
-INSERT INTO ModifierArguments 
-       (ModifierId,                         Name,     Value) 
-VALUES ('MOD_NYGUITA_LIGA_DE_GOBERNADORES', 'Amount', '5');
-
-INSERT INTO ModifierArguments 
-       (ModifierId,                         Name,        Value) 
-VALUES ('MOD_NYGUITA_LIGA_DE_GOBERNADORES', 'CivUnique', 'True');
+INSERT INTO ModifierStrings 
+       (ModifierId,              Context,       Text) 
+VALUES ('MOD_NYGUITA_LIGA_DE_GOBERNADORES', 'Description', 'LOC_MOD_NYGUITA_LIGA_DE_GOBERNADORES_DESCRIPTION');
 
 
 -- CIVIC_NYGUITA_TANGO_AND_POETRY
@@ -160,31 +164,20 @@ VALUES ('MOD_NYGUITA_FLORIDA_AND_BOEDO', 'MOD_NYGUITA_FLORIDA_AND_BOEDO_TYPE');
 
 INSERT INTO ModifierArguments 
        (ModifierId,                      Name,        Value) 
-VALUES ('MOD_NYGUITA_FLORIDA_AND_BOEDO', 'CivUnique', 'True');
+VALUES ('MOD_NYGUITA_FLORIDA_AND_BOEDO', 'CivUnique', 'True'),
+       ('MOD_NYGUITA_FLORIDA_AND_BOEDO', 'Invert', 'True'),
+       ('MOD_NYGUITA_FLORIDA_AND_BOEDO', 'Amount', '5'),
+       ('MOD_NYGUITA_FLORIDA_AND_BOEDO', 'YieldType', 'YIELD_CULTURE');
 
-INSERT INTO ModifierArguments 
-       (ModifierId,                      Name,     Value) 
-VALUES ('MOD_NYGUITA_FLORIDA_AND_BOEDO', 'Invert', 'True');
-
-INSERT INTO ModifierArguments 
-       (ModifierId,                      Name,     Value) 
-VALUES ('MOD_NYGUITA_FLORIDA_AND_BOEDO', 'Amount', '5');
-
-INSERT INTO ModifierArguments 
-       (ModifierId,                      Name,        Value) 
-VALUES ('MOD_NYGUITA_FLORIDA_AND_BOEDO', 'YieldType', 'YIELD_CULTURE');
+INSERT INTO ModifierStrings 
+       (ModifierId,              Context,       Text) 
+VALUES ('MOD_NYGUITA_FLORIDA_AND_BOEDO', 'Description', 'LOC_MOD_NYGUITA_FLORIDA_AND_BOEDO_DESCRIPTION');
 
 INSERT INTO ProgressionTreeNodeUnlocks 
        (ProgressionTreeNodeType,          TargetType,                      Hidden, TargetKind,           UnlockDepth) 
-VALUES ('CIVIC_NYGUITA_TANGO_AND_POETRY', 'WONDER_NYGUITA_PALACIO_BAROLO', 0,      'KIND_CONSTRUCTIBLE', 1);
-
-INSERT INTO ProgressionTreeNodeUnlocks 
-       (ProgressionTreeNodeType,          TargetType,                      Hidden, TargetKind,       UnlockDepth) 
-VALUES ('CIVIC_NYGUITA_TANGO_AND_POETRY', 'TRADITION_NYGUITA_NUEVO_TANGO', 0,      'KIND_TRADITION', 2);
-
-INSERT INTO ProgressionTreeNodeUnlocks 
-       (ProgressionTreeNodeType,          TargetType,                                Hidden, TargetKind,      UnlockDepth) 
-VALUES ('CIVIC_NYGUITA_TANGO_AND_POETRY', 'MOD_NYGUITA_GIANTS_OF_PATAGONIA_SCIENCE', 0,      'KIND_MODIFIER', 2);
+VALUES ('CIVIC_NYGUITA_TANGO_AND_POETRY', 'WONDER_NYGUITA_PALACIO_BAROLO', 0,      'KIND_CONSTRUCTIBLE', 1),
+       ('CIVIC_NYGUITA_TANGO_AND_POETRY', 'TRADITION_NYGUITA_NUEVO_TANGO', 0,      'KIND_TRADITION', 2),
+       ('CIVIC_NYGUITA_TANGO_AND_POETRY', 'MOD_NYGUITA_GIANTS_OF_PATAGONIA_SCIENCE', 0,      'KIND_MODIFIER', 2);
 
 
 -- MOD_NYGUITA_GIANTS_OF_PATAGONIA_SCIENCE
@@ -202,15 +195,16 @@ VALUES ('MOD_NYGUITA_GIANTS_OF_PATAGONIA_SCIENCE', 'MOD_NYGUITA_GIANTS_OF_PATAGO
 
 INSERT INTO ModifierArguments 
        (ModifierId,                                Name,     Value) 
-VALUES ('MOD_NYGUITA_GIANTS_OF_PATAGONIA_SCIENCE', 'Amount', '3');
+VALUES ('MOD_NYGUITA_GIANTS_OF_PATAGONIA_SCIENCE', 'Amount', '3'),
+       ('MOD_NYGUITA_GIANTS_OF_PATAGONIA_SCIENCE', 'YieldType', 'YIELD_SCIENCE');
 
-INSERT INTO ModifierArguments 
-       (ModifierId,                                Name,        Value) 
-VALUES ('MOD_NYGUITA_GIANTS_OF_PATAGONIA_SCIENCE', 'YieldType', 'YIELD_SCIENCE');
+INSERT INTO ModifierStrings 
+       (ModifierId,              Context,       Text) 
+VALUES ('MOD_NYGUITA_GIANTS_OF_PATAGONIA_SCIENCE', 'Description', 'LOC_MOD_NYGUITA_GIANTS_OF_PATAGONIA_DESCRIPTION');
 
 INSERT INTO ProgressionTreeNodeUnlocks 
        (ProgressionTreeNodeType,          TargetType,                                   Hidden, TargetKind,      UnlockDepth) 
-VALUES ('CIVIC_NYGUITA_TANGO_AND_POETRY', 'MOD_NYGUITA_GIANTS_OF_PATAGONIA_PRODUCTION', 0,      'KIND_MODIFIER', 2);
+VALUES ('CIVIC_NYGUITA_TANGO_AND_POETRY', 'MOD_NYGUITA_GIANTS_OF_PATAGONIA_PRODUCTION', 1,      'KIND_MODIFIER', 2);
 
 
 -- MOD_NYGUITA_GIANTS_OF_PATAGONIA_PRODUCTION
@@ -228,19 +222,13 @@ VALUES ('MOD_NYGUITA_GIANTS_OF_PATAGONIA_PRODUCTION', 'MOD_NYGUITA_GIANTS_OF_PAT
 
 INSERT INTO ModifierArguments 
        (ModifierId,                                   Name,      Value) 
-VALUES ('MOD_NYGUITA_GIANTS_OF_PATAGONIA_PRODUCTION', 'Percent', '30');
-
-INSERT INTO ModifierArguments 
-       (ModifierId,                                   Name,       Value) 
-VALUES ('MOD_NYGUITA_GIANTS_OF_PATAGONIA_PRODUCTION', 'UnitType', 'UNIT_EXPLORER');
+VALUES ('MOD_NYGUITA_GIANTS_OF_PATAGONIA_PRODUCTION', 'Percent', '30'),
+       ('MOD_NYGUITA_GIANTS_OF_PATAGONIA_PRODUCTION', 'UnitType', 'UNIT_EXPLORER');
 
 INSERT INTO ProgressionTreePrereqs 
        (Node,                                       PrereqNode) 
-VALUES ('CIVIC_NYGUITA_CIVILIZATION_AND_BARBARISM', 'CIVIC_NYGUITA_CONGRESS_OF_TUCUMAN');
-
-INSERT INTO ProgressionTreePrereqs 
-       (Node,                             PrereqNode) 
-VALUES ('CIVIC_NYGUITA_TANGO_AND_POETRY', 'CIVIC_NYGUITA_CONGRESS_OF_TUCUMAN');
+VALUES ('CIVIC_NYGUITA_CIVILIZATION_AND_BARBARISM', 'CIVIC_NYGUITA_CONGRESS_OF_TUCUMAN'),
+       ('CIVIC_NYGUITA_TANGO_AND_POETRY', 'CIVIC_NYGUITA_CONGRESS_OF_TUCUMAN');
 
 -- Quotes
 INSERT INTO TypeQuotes
