64d01fef510de3c33d4c399715b0ed6580892dda
Desmony <33091422+Desmony@users.noreply.github.com>
2025-09-10 16:15:41 +0200

    Update images.sql

--- civilizations/nyguita-argentina/images.sql
+++ civilizations/nyguita-argentina/images.sql
@@ -6,4 +6,4 @@ INSERT INTO IconDefinitions
        (ID,                               Context,   IconSize, Path) 
 VALUES ('CIVILIZATION_NYGUITA_ARGENTINA', 'DEFAULT', 0,        'fs://game/nyguita-argentina/argentina_civ_icon.png'),
        ('ICON_CIVILIZATION_NYGUITA_ARGENTINA', 'DEFAULT', 0,        'fs://game/nyguita-argentina/argentina_civ_icon.png'),
-       ('civ_line_nyguita_argentina', 'DEFAULT', 0,        'fs://game/nyguita-argentina/argentina_civ_icon.png');
+       ('civ_line_nyguita_argentina', 0, 0,        'fs://game/nyguita-argentina/argentina_civ_icon.png');
