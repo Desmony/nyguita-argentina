1563cde4deccefbba9a0c7f3719c906158682d9b
Desmony <33091422+Desmony@users.noreply.github.com>
2025-09-11 13:57:38 +0200

    Revert "Update images.sql"
    This reverts commit 458d58bb05ba79824558f52feb962e421d4d3299.

--- civilizations/nyguita-argentina/images.sql
+++ civilizations/nyguita-argentina/images.sql
@@ -4,8 +4,8 @@ VALUES ('CIVILIZATION_NYGUITA_ARGENTINA', 'DEFAULT');
 
 INSERT INTO IconDefinitions 
        (ID,                               , Path) 
-VALUES ('CIVILIZATION_NYGUITA_ARGENTINA',        'fs://game/nyguita-argentina/argentina_civ_icon.png'),
-       ('ICON_CIVILIZATION_NYGUITA_ARGENTINA',         'fs://game/nyguita-argentina/argentina_civ_icon.png'),
+VALUES ('CIVILIZATION_NYGUITA_ARGENTINA',        'fs://game/nyguita-argentina/civ_sym_nyguita_argentina'),
+       ('ICON_CIVILIZATION_NYGUITA_ARGENTINA',         'fs://game/nyguita-argentina/civ_sym_nyguita_argentina'),
        ('civ_line_nyguita_argentina',        'fs://game/nyguita-argentina/civ_sym_nyguita_argentinan'),
        ('civ_sym_nyguita_argentina',        'fs://game/nyguita-argentina/civ_sym_nyguita_argentina'),
 	('dip_cs_nyguita_argentina',				'fs://game/nyguita-argentina/dip_cs_nyguita_argentina');
