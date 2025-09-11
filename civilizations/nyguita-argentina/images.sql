71a62c4ba3c86c2a6c9769b1a5582649b84610dd
Desmony <33091422+Desmony@users.noreply.github.com>
2025-09-11 14:22:55 +0200

    Fixed Plaza de Mayo requirements

--- civilizations/nyguita-argentina/images.sql
+++ civilizations/nyguita-argentina/images.sql
@@ -3,7 +3,7 @@ INSERT INTO Icons
 VALUES ('CIVILIZATION_NYGUITA_ARGENTINA', 'DEFAULT');
 
 INSERT INTO IconDefinitions 
-       (ID,                               , Path) 
+       (ID,                               Path) 
 VALUES ('CIVILIZATION_NYGUITA_ARGENTINA',        'fs://game/nyguita-argentina/civ_sym_nyguita_argentina'),
        ('ICON_CIVILIZATION_NYGUITA_ARGENTINA',         'fs://game/nyguita-argentina/civ_sym_nyguita_argentina'),
        ('civ_line_nyguita_argentina',        'fs://game/nyguita-argentina/civ_sym_nyguita_argentinan'),
