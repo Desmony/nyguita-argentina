5b263f2287cd9e1dcb7a757dd8fd6940a863532d
Desmony <33091422+Desmony@users.noreply.github.com>
2025-09-11 22:28:27 +0200

    Added special thanks

--- units/nyguita-porteno/localizations.sql
+++ units/nyguita-porteno/localizations.sql
@@ -1,6 +1,6 @@
 INSERT OR IGNORE INTO LocalizedText 
        (Language, Tag,                             Text) 
 VALUES ('en_US',  'LOC_UNIT_NYGUITA_PORTENO_NAME', 'Porteño'),
-       ('en_US',  'LOC_UNIT_NYGUITA_PORTENO_DESCRIPTION', 'Argentinian Unique Settler. Civilian Unit capable of founding new Towns. Adds +3 [icon:YIELD_Food]Food per Coastal terrain within 1 tile of the city center on the City Hall.'),
-       ('en_US',  'LOC_UNIT_NYGUITA_PORTENO_TOOLTIP', '+3 [icon:YIELD_Food]Food per Coastal terrain within 1 tile of the city center on the City Hall.'),
+       ('en_US',  'LOC_UNIT_NYGUITA_PORTENO_DESCRIPTION', 'Argentinian Unique Settler. Civilian Unit capable of founding new Towns. Adds +3 [icon:YIELD_Food] Food per Coastal terrain within 1 tile of the city center on the City Hall.'),
+       ('en_US',  'LOC_UNIT_NYGUITA_PORTENO_TOOLTIP', '+3 [icon:YIELD_Food] Food per Coastal terrain within 1 tile of the city center on the City Hall.'),
        ('en_US',  'LOC_PEDIA_PAGE_UNIT_NYGUITA_PORTENO_CHAPTER_HISTORY_PARA_1', 'Porteño (literraly "port city person" in Spanish) refers to residents of port cities in various hispanic countries but specifically Buenos Aires. It also refers to the descendants of the Great European Migration Wave to Argentina from the second half of the 19th century, in opposition to the descendants of the Spanish colonists of the 16th century and to the native Americans.');
