735d29a5d0465269902af3865cc6ce572b664996
Desmony <33091422+Desmony@users.noreply.github.com>
2025-09-11 10:53:23 +0200

    Addad dip_cs_nyguita_argentina

--- civilizations/nyguita-argentina/images.sql
+++ civilizations/nyguita-argentina/images.sql
@@ -3,7 +3,8 @@ INSERT INTO Icons
 VALUES ('CIVILIZATION_NYGUITA_ARGENTINA', 'DEFAULT');
 
 INSERT INTO IconDefinitions 
-       (ID,                               Context,   IconSize, Path) 
-VALUES ('CIVILIZATION_NYGUITA_ARGENTINA', 'DEFAULT', 0,        'fs://game/nyguita-argentina/argentina_civ_icon.png'),
-       ('ICON_CIVILIZATION_NYGUITA_ARGENTINA', 'DEFAULT', 0,        'fs://game/nyguita-argentina/argentina_civ_icon.png'),
-       ('civ_line_nyguita_argentina', 0, 0,        'fs://game/nyguita-argentina/argentina_civ_icon.png');
+       (ID,                               , Path) 
+VALUES ('CIVILIZATION_NYGUITA_ARGENTINA',        'fs://game/nyguita-argentina/argentina_civ_icon.png'),
+       ('ICON_CIVILIZATION_NYGUITA_ARGENTINA',         'fs://game/nyguita-argentina/argentina_civ_icon.png'),
+       ('civ_line_nyguita_argentina',        'fs://game/nyguita-argentina/argentina_civ_icon.png');
+	('dip_cs_nyguita_argentina',				'fs://game/nyguita-argentina/dip_cs_nyguita_argentina.png');
