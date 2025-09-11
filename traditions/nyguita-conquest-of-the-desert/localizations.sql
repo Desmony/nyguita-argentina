4f52ace20a47e55d80888bcd35f2e698dd0c371f
Desmony <33091422+Desmony@users.noreply.github.com>
2025-09-11 12:21:20 +0200

    Fixed Conquest of the Desert

--- traditions/nyguita-conquest-of-the-desert/localizations.sql
+++ traditions/nyguita-conquest-of-the-desert/localizations.sql
@@ -2,4 +2,5 @@ INSERT OR IGNORE INTO LocalizedText
        (Language, Tag,                                                                   Text) 
 VALUES ('en_US',  'LOC_MOD_TRADITION_NYGUITA_CONQUEST_OF_THE_DESERT_COMBAT_DESCRIPTION', '+5 Combat Strength against Independent Powers.'),
        ('en_US',  'LOC_TRADITION_NYGUITA_CONQUEST_OF_THE_DESERT_NAME', 'Conquest of the Desert'),
-       ('en_US',  'LOC_TRADITION_NYGUITA_CONQUEST_OF_THE_DESERT_DESCRIPTION', '+5 Combat Strength against Independent Powers.');
+       ('en_US',  'LOC_TRADITION_NYGUITA_CONQUEST_OF_THE_DESERT_DESCRIPTION', '+5 Combat Strength against Independent Powers.'),
+       ('en_US',  'LOC_NYGUITA_CONQUEST_OF_THE_DESERT_ABILITY_PREVIEW', 'Against Independent Powers: +5');
