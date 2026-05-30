6714633bf76cffbcbe282a10eb2696731764056b
Desmony <33091422+Desmony@users.noreply.github.com>
2026-05-30 20:12:44 +0200

    Added compatibility with Art fixes (mod that doesn't exist yet)

---
+++ civ-icon-fixes/fixes.sql
@@ -0,0 +1,3 @@
+INSERT INTO CivsWithoutBackgrounds
+        (CivilizationType,              ArtPath)
+VALUES ('CIVILIZATION_NYGUITA_ARGENTINA', 'fs://game/nyguita-argentina/civilizations/nyguita-argentina/assets/argentina_background_1080.png');
