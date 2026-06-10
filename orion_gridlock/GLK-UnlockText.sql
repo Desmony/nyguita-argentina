b4e5df6f9826d189818c38f4496a02938b6d9c7c
Desmony <33091422+Desmony@users.noreply.github.com>
2026-06-10 21:57:05 +0200

    Gridlock compatibility (again)

--- orion_gridlock/GLK-UnlockText.sql
+++ orion_gridlock/GLK-UnlockText.sql
@@ -4,3 +4,4 @@ VALUES
 ("GRIDLOC_Pacha_NARG_TOOLTIP","en_US","[B]Pachacuti[/B] and [B]Argentina[/B] both belong to the [B]South America[/B] hex."),
 ("GRIDLOC_Boliv_NARG_TOOLTIP","en_US","[B]Simón Bolívar[/B] and [B]Argentina[/B] both belong to the [B]South America[/B] hex."),
 ("GRIDLOC_NARG_DESCRIPTION","en_US","This [B]Modern Age[/B] Civ belongs to the [B]South America[/B] Hex.");
+
